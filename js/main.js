a6d3db023869d278221b6ec7f7947a3803fbfdfb
ua-kabak <kabak05012001@gmail.com>
2022-08-14 12:32:12 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@ $(function () {
     $('.nav__list').toggleClass('nav__list--active');
     $('.body').toggleClass('body--active');
   });
-  
+
+  $('.nav__link--mobile').on('click', function () {
+    $('.header__btn').toggleClass('header__btn--active');
+    $('.nav__list').toggleClass('nav__list--active');
+    $('.body').toggleClass('body--active');
+  })
+
+
   $(".logo, .nav__link").on("click", function (e) {
     e.preventDefault();
 
@@ -16,4 +23,13 @@ $(function () {
       scrollTop: top
     }, 1000);
   });
+
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animate__animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  })
+  wow.init();
 });
